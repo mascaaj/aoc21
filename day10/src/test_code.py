a5d37b6c4aec3a33ea72146a49ba25c0f018ea36
mascaaj <alokejm@gmailcom>
2021-12-10 12:49:33 +0530

    Merge day10 solutions

---
+++ day10/src/test_code.py
@@ -0,0 +1,22 @@
+"""
+Day X puzzle unit Testing
+"""
+# from dayX.src.puzzle2 import xyz, abc
+
+# def test_xyz():
+#     """
+#     Day X puzzle unit Testing
+#     """
+#     expected = [1, 3, 1]
+#     actual = [1, 3, 1]
+#     print(actual)
+#     assert len(expected) == len(actual)
+#     assert all([a == b for a, b in zip(actual, expected)])
+#     print(all([a == b for a, b in zip(actual, expected)]))
+
+# def test_abc():
+#     """
+#     Day X puzzle unit Testing
+#     """
+
+#     assert
