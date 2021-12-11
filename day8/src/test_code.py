0dd060d2040dd0a4b5cc4262a502b48f77f2fea4
mascaaj <alokejm@gmailcom>
2021-12-11 15:25:07 +0530

    Merge day11 solutions

--- day8/src/test_code.py
+++ day8/src/test_code.py
@@ -1,12 +1,12 @@
-#Figuring out dictionary usage
+# Figuring out dictionary usage
 
-dict = {'key1':'geeks', 'key2':'for'} 
-print("Current Dict is: ", dict) 
-  
-# adding dict1 (key3, key4 and key5) to dict 
-dict1 = {'key3':'geeks', 'key4':'is', 'key5':'fabulous'} 
-dict.update(dict1) 
-  
-# by assigning 
-dict.update(newkey1 ='portal') 
-print(dict) 
+dict = {'key1': 'geeks', 'key2': 'for'}
+print("Current Dict is: ", dict)
+
+# adding dict1 (key3, key4 and key5) to dict
+dict1 = {'key3': 'geeks', 'key4': 'is', 'key5': 'fabulous'}
+dict.update(dict1)
+
+# by assigning
+dict.update(newkey1='portal')
+print(dict)
